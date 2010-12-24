38a50a0dc15f6ac6116a54196734ae5d23e9185a
Nishibayashi Takuji <takuji@senchan.jp>
2010-12-24 17:23:07 +0900

    initial commit

---
+++ lib/Plack/Middleware/Auth/OAuth/Lite.pm
@@ -0,0 +1,32 @@
+package Plack::Middleware::Auth::OAuth::Lite;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::Middleware::Auth::OAuth::Lite -
+
+=head1 SYNOPSIS
+
+  use Plack::Middleware::Auth::OAuth::Lite;
+
+=head1 DESCRIPTION
+
+Plack::Middleware::Auth::OAuth::Lite is
+
+=head1 AUTHOR
+
+Nishibayashi Takuji E<lt>takuji {at} senchan.jpE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
