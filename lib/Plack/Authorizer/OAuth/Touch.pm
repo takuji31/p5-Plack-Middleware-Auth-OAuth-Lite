6c343fd60e10d288fcc28cea000ebeeefd3eed92
Nishibayashi Takuji <takuji@senchan.jp>
2011-01-18 10:36:22 +0900

    パラメーターの解析にHash::MultiValueを使うようにする

--- lib/Plack/Authorizer/OAuth/Touch.pm
+++ lib/Plack/Authorizer/OAuth/Touch.pm
@@ -4,6 +4,7 @@ use warnings;
 use parent qw/Plack::Authorizer::OAuth::Base/;
 
 use Plack::Session;
+use Hash::MultiValue;
 
 our @REQUIRED_PARAMETERS = qw/
     oauth_consumer_key
@@ -19,41 +20,56 @@ our @REQUIRED_PARAMETERS = qw/
 our $SESSION_KEY = 'oauth_session_info';
 
 sub authorize {
-    my ($class, $middleware, $env) = @_;
+    my ( $class, $middleware, $env ) = @_;
 
     my $req = $class->create_request($env);
 
     my $do_auth = 1;
 
     #parameter check
-    map { $do_auth = $do_auth && $req->param($_)} @REQUIRED_PARAMETERS;
+    map { $do_auth = $do_auth && $req->param($_) } @REQUIRED_PARAMETERS;
 
     my $session = Plack::Session->new($env);
-    unless($do_auth) {
+    unless ($do_auth) {
+
         #get session
         return $session->get($SESSION_KEY);
     }
 
-    my $params = $class->parse_auth_header($env);
+    my $auth_params = $class->parse_auth_header($env);
 
     #XXX get only?
-    my $req_params = $middleware->validate_post ? $req->parameters : $req->query_parameters;
+    my $params = $middleware->validate_post
+        ? $req->parameters->clone
+        : $req->query_parameters->clone;
 
-    map { $params->{$_} = $req_params->{$_} } $req_params->keys;
+    while ( my ( $key, $value ) = each %$auth_params ) {
+        if ( ref($value) eq 'ARRAY' ) {
+            for my $v (@$value) {
+                $params->add( $key => $v );
+            }
+        }
+        else {
+            $params->add( $key => $value );
+        }
+
+    }
 
     my $result = $class->verify_hmac_sha1(
         {
             method          => $req->method,
             url             => $req->uri,
-            params          => $params,
+            params          => $params->as_hashref_mixed,
             consumer_secret => $middleware->consumer_secret,
             token_secret    => $params->{oauth_token_secret},
         }
     );
 
-    if($result) {
+    if ($result) {
+
         #regenerate session id
         $session->options->{change_id}++;
+
         #store session
         $session->set( $SESSION_KEY, $params );
     }
