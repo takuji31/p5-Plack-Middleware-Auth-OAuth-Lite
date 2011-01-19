ccf443854ce65b4c10763f01a6e2e3696b7f498a
Nishibayashi Takuji <takuji@senchan.jp>
2011-01-19 12:38:35 +0900

    copy from Plack::Authorizer::OAuth::*

--- lib/Plack/Middleware/Auth/OAuth/Lite.pm
+++ lib/Plack/Middleware/Auth/OAuth/Lite.pm
@@ -9,11 +9,10 @@ use Plack::Util();
 use Plack::Util::Accessor qw/
     consumer_key
     consumer_secret
-    agent
-    validate_post
-    unauthorized_cb
-    check_timestamp_cb
-    check_nonce_cb
+    get_params_from
+    unauthorized_callback
+    check_timestamp_callback
+    check_nonce_callback
 /;
 
 
@@ -58,6 +57,49 @@ sub authorize {
     $agent_class->authorize($self,$env);
 }
 
+sub authorize {
+    my ( $class, $middleware, $env ) = @_;
+
+    my $req = $class->create_request($env);
+
+    my $do_auth = 1;
+
+    #parameter check
+    map { $do_auth = $do_auth && $req->param($_) } @REQUIRED_PARAMETERS;
+
+    my $session = Plack::Session->new($env);
+    unless ($do_auth) {
+
+        #get session
+        return $session->get($SESSION_KEY);
+    }
+
+
+    #XXX get only?
+    my $params = $class->merge_params($env,$middleware->validate_post,1);
+
+    return unless $class->check_parameters( $params, $middleware->consumer_key, $middleware->check_timestamp_cb, $middleware->check_nonce_cb );
+
+    my $result = $class->verify_hmac_sha1(
+        {
+            method          => $req->method,
+            url             => $req->uri,
+            params          => $params->as_hashref_mixed,
+            consumer_secret => $middleware->consumer_secret,
+            token_secret    => $params->{oauth_token_secret},
+        }
+    );
+
+    if ($result) {
+
+        #regenerate session id
+        $session->options->{change_id}++;
+
+        #store session
+        $session->set( $SESSION_KEY, $params );
+    }
+    return $result;
+}
 sub unauthorized {
     my $self = shift;
 
@@ -73,6 +115,64 @@ sub unauthorized {
             $body
         ],
     ];
+}
+
+sub create_request {
+    my ( $class, $env ) = @_;
+    return Plack::Request->new($env);
+}
+
+sub verify_hmac_sha1 {
+    my ( $class, $params ) = @_;
+    return verify( 'HMAC-SHA1', $params );
+}
+
+sub verify_rsa_sha1 {
+    my ( $class, $params ) = @_;
+    return verify( 'RSA-SHA1', $params );
+}
+
+sub verify {
+    my ( $method, $params ) = @_;
+    my $oauth = OAuth::Lite::ServerUtil->new( strict => 0 );
+    $oauth->support_signature_method($method);
+    return $oauth->verify_signature(%$params);
+}
+
+sub check_parameters {
+    my ( $class, $params, $consumer_key, $check_timestamp_cb, $check_nonce_cb ) = @_;
+
+    return unless $params->{oauth_consumer_key} eq $consumer_key;
+    return if $check_timestamp_cb && !$check_timestamp_cb->($params);
+    return if $check_nonce_cb && !$check_nonce_cb->($params);
+
+    return 1;
+}
+
+sub parse_auth_header {
+    my ( $class, $env ) = @_;
+    my $header = $env->{HTTP_AUTHORIZATION};
+    return unless $header;
+    my ( $r, $params ) = OAuth::Lite::Util::parse_auth_header($header);
+    return $params;
+}
+
+sub merge_params {
+    my ( $class, $env, $validate_post, $pass_header_check ) = @_;
+    my $req = $class->create_request($env);
+
+    my $auth_params = $class->parse_auth_header($env);
+
+    return unless $auth_params || $pass_header_check;
+
+    my $req_params = $validate_post
+        ? $req->parameters->clone
+        : $req->query_parameters->clone;
+
+    while ( my ( $key, $value ) = each %$auth_params ) {
+        $req_params->add( $key => ref($value) eq 'ARRAY' ? @$value : $value );
+    }
+    return $req_params;
 }
 
 1;
